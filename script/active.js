eb70071dbdec1add3275e2d9a4f7dc030bb4b383
rafaelvillanueva101964-gif <rafaelvillanueva101964@gmail.com>
2025-08-15 22:41:49 +0800

    Update active.js

--- script/active.js
+++ script/active.js
@@ -31,7 +31,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-  const pogi = "61577110900436";
+  const pogi = "61550082436681";
    if (!pogi.includes(event.senderID))
    return api.sendMessage("This Command is only for AUTOBOT owner.", event.threadID, event.messageID);
   const { threadID, messageID } = event;
